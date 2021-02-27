e2d81a9280300103dea7c351ee5e6b285380e5ba
ThiagoPiccioni <thiagopiccioni@gmail.com>
2021-02-27 12:27:30 +0000

    deployment

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ from send_mail import send_mail
 
 app = Flask(__name__)
 
-ENV = 'dev'
+ENV = 'prod'
 
 if ENV == 'dev':
     app.debug = True
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456@localhost/lexus'
 else:
     app.debug = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://lwhubctcalnxto:6b86f56a4bf967fd803c70967a220b7e73037cc7182ed0bf608aa58c4701bd27@ec2-54-155-87-214.eu-west-1.compute.amazonaws.com:5432/d3htqfpq4be1gj'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
